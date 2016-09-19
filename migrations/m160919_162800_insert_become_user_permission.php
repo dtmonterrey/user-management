a4c78f9e1f894c9becbceaeffc62f85d6ea9a6ac
Evandro Pires Alves <evandro@ipb.pt>
2016-09-19 18:06:41 +0100

    Another fix for the migration

--- migrations/m160919_162800_insert_become_user_permission.php
+++ migrations/m160919_162800_insert_become_user_permission.php
@@ -1,6 +1,7 @@
 <?php
 
 use webvimark\modules\UserManagement\models\rbacDB\Permission;
+use webvimark\modules\UserManagement\models\rbacDB\Route;
 use yii\db\Migration;
 
 class m160919_162800_insert_become_user_permission extends Migration
